fa51a8c08e625fa28e9db900ea13b80cdbc55b7e
传说故事 <1114976722@qq.com>
2022-03-25 17:30:13 +0800

    打包函数

--- utils.py
+++ utils.py
@@ -17,7 +17,9 @@ def reshape_img(img):
     return img
 
 def gen_lr(hr):
-    """将HR图片生成2张LR图片"""
+    """将HR图片下采样,生成2张LR图片,用于亚像素级别数据集生成
+    hr.size=(2m,2n)
+    lr1.size=lr2.size=(m-1,n-1)"""
     lr1 = np.zeros((int(hr.shape[0] / 2) - 1, int(hr.shape[1] / 2) - 1))  # 初始化低分辨率图像lr1
     lr2 = np.zeros_like(lr1)  # 初始化低分辨率图像lr2
     for i in range(lr1.shape[0]):
@@ -29,3 +31,19 @@ def gen_lr(hr):
     return lr1, lr2
 
 
+def total_inter(lr_l, lr_d):
+    """全插值法将左移0.5和下移0.5的图片进行插值。也就是左上和右下插值，同超模式采样数据。得到HR。
+    lr_left.size=lr_down.size=(m,n)
+    hr.size=(2m-1,2n-1)
+    """
+    hr = np.zeros((lr_l.shape[0] * 2 - 1, lr_l.shape[1] * 2 - 1))
+    for i in range(lr_l.shape[0]):
+        for j in range(lr_l.shape[1]):
+            hr[2 * i][2 * j] = (int(lr_l[i][j]) + int(lr_d[i][j])) / 2
+            if j != lr_l.shape[1] - 1:
+                hr[2 * i][2 * j + 1] = (int(lr_l[i][j + 1]) + int(lr_d[i][j])) / 2
+            if i != lr_l.shape[0] - 1:
+                hr[2 * i + 1][2 * j] = (int(lr_l[i + 1][j]) + int(lr_d[i][j])) / 2
+            if j != lr_l.shape[1] - 1 and i != lr_l.shape[0] - 1:
+                hr[2 * i + 1][2 * j + 1] = (int(lr_l[i + 1][j + 1]) + int(lr_d[i][j])) / 2
+    return hr
